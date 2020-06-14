d7ddc3b7238624330b1d26c2f815f87ecb5a665a
Adhitya-Raj <adhitya@Adhitya-Lucifer.localdomain>
2020-06-14 23:15:21 +0530

    omdb api working. Results shown as list.

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
+const { response } = require('express');
+const { json } = require('body-parser');
+
 var app = require('express')(),
-    port = process.env.PORT || "3000",
-    ip = process.env.IP || "172.27.154.145";
+    bodyParser = require('body-parser'),
+    got = require('got'),
+    port = process.env.PORT || "3000";
+
+app.set("view engine","ejs");
 
 app.get("/",(req,res)=>{
-    res.send("App is working!");
+    res.render("index");
 });
 
-app.listen(port,ip,()=>{
-    console.log("Server Running at : " + ip + ":" + port + "/");
+app.get("/search",(req,res)=>{
+    (async () => {
+        try {
+            const response = await got('http://www.omdbapi.com/?apikey=8bac5ca1&s='+req.query.title);
+            var data = JSON.parse(response.body);
+            // console.log(data);
+            // res.send(data);
+            res.render("results",{results: data.Search});
+            //=> '<!doctype html> ...'
+        } catch (error) {
+            console.log(error.response.body);
+            res.redirect("/");
+            //=> 'Internal server error ...'
+        }
+    })();
+});
+
+app.listen(port,()=>{
+    console.log("Server Running at : https://localhost:" + port + "/");
 });
